3891fb968094d9d6444c844f263067cfea5e2d8e
Djalo08 <8230592@estg.ipp.pt>
2026-03-11 14:01:39 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,19 @@
-    <script>
-        const openweathermaps_key = "f002dfd443e7f397e71917a53cccaecc"
+       const openweathermaps_key = "f002dfd443e7f397e71917a53cccaecc"
 
-        function searchWeather(lat, long) {
-            var lat = $("#paw-form-lat").val();
-            var lon = document.getElementById('paw-form-lon').value;
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    var weatherObject = JSON.parse(xhttp.response);
-                    var currentWeather = weatherObject["weather"][0]["description"];
-                    $("#paw-results-row").show();
-                    document.getElementById("Results").innerHTML = currentWeather;
-                }
-            }
-
-            xhttp.open("GET", `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${openweathermaps_key}`, true);
-            xhttp.setRequestHeader('Accept', 'application/json');
-            xhttp.send();
+       function searchWeather() {
+    var lat = $("#paw-form-lat").val();
+    var lon = $("#paw-form-lon").val();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var weatherObject = JSON.parse(xhttp.response);
+            var currentWeather = weatherObject["weather"][0]["description"];
+            $("#paw-results-row").show();
+            $("#Results").html(currentWeather);
         }
-    </script>
+    }
+    xhttp.open("GET",`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${openweathermaps_key}`,true);
+    xhttp.setRequestHeader('Accept', 'application/json');
+    xhttp.send();
+        }
 //alterei a linha 12,5 e o meu key
