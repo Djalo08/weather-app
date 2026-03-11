fb71b2521681ce64f6bd7423bd2593fc4e26dbb1
Djalo08 <8230592@estg.ipp.pt>
2026-03-11 14:18:57 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
        const openweathermaps_key = "f002dfd443e7f397e71917a53cccaecc"
-       document.getElementById("paw-form-searchWeather").addEventListener("click", searchWeather);
+        function searchWeather(lat, long) {
+            var lat = document.getElementById('paw-form-lat').value;
+            var lon = document.getElementById('paw-form-lon').value;
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    var weatherObject = JSON.parse(xhttp.response);
+                    var currentWeather = weatherObject["weather"][0]["description"];
+                    document.getElementById("paw-results-row").style.display = "block";
+                    document.getElementById("Results").innerHTML = currentWeather;
+                }
+            }
 
-       function searchWeather() {
-    var lat = $("#paw-form-lat").val();
-    var lon = $("#paw-form-lon").val();
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var weatherObject = JSON.parse(xhttp.response);
-            var currentWeather = weatherObject["weather"][0]["description"];
-            $("#paw-results-row").show();
-            $("#Results").html(currentWeather);
+            xhttp.open("GET", `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${openweathermaps_key}`, true);
+            xhttp.setRequestHeader('Accept', 'application/json');
+            xhttp.send();
         }
-    }
-    xhttp.open("GET",`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${openweathermaps_key}`,true);
-    xhttp.setRequestHeader('Accept', 'application/json');
-    xhttp.send();
-        }
+
 //alterei a linha 12,5 e o meu key
